5c875f012e241f0c3a5631470c819c53f038e3fe
MortalSC <mortal_scode@163.com>
2023-08-24 20:37:56 +0800

    服务端：引用智能指针管理

--- MinHttp/main.cpp
+++ MinHttp/main.cpp
@@ -1,20 +1,39 @@
 #include <iostream>
 #include <string>
-#include "TcpServer.hpp"
+#include <memory> // 引入智能指针管理！
+// #include "TcpServer.hpp"
+#include "HttpServer.hpp"
 
-static void Usage(std::string proc){
+static void Usage(std::string proc)
+{
     std::cout << "Usage =>\t" << proc << " port" << std::endl;
 }
 
-int main(int argc, char* argv[]){
+int main(int argc, char *argv[])
+{
 
-    if(argc != 2){
+    if (argc != 2)
+    {
         Usage(argv[0]);
         exit(4);
     }
-
+    /* 获取用户指定的端口号 */
     uint16_t port = atoi(argv[1]);
+
+    /* version 2 : testing HttpServer.hpp create a task */
+    std::shared_ptr<HttpServer> http_server(new HttpServer(port));
+    /* 初始化服务 */
+    http_server->InitHttpServer();
+    /* 启动服务 */
+    http_server->Loop();
+
+/* version 1 : testing TcpServer.hpp create a server */
+#if 0
     TcpServer* Tcptr = TcpServer::GetInstance(port);
+#endif
+    while (true)
+    {
+    }
 
     return 0;
 }
