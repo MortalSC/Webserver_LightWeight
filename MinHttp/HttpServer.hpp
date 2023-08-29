fd125d8b0a6e8f3e9734fcb42b6387ffffdb3689
MortalSC <mortal_scode@163.com>
2023-08-29 22:26:52 +0800

    引入线程池管理

--- MinHttp/HttpServer.hpp
+++ MinHttp/HttpServer.hpp
@@ -3,29 +3,50 @@
 #include <iostream>
 #include <pthread.h>
 #include "TcpServer.hpp"
-#include "Protocol.hpp"
+// #include "Protocol.hpp"
 #include "Log.hpp"
+/* 信号 */
+#include <signal.h>
+#include "Task.hpp"
+#include "ThreadPool.hpp"
 
 /* 设置默认端口号 */
 #define PORT 8080
 
 class HttpServer
 {
+private:
+    /* 端口号 */
+    uint16_t _port;
+    /* 服务是否可停止 */
+    bool _stop;
+
+    // /* 网络服务实例 */
+    // TcpServer *_tcp_server;
+
+
+    // /* 线程池对象 */
+    // ThreadPool _threadpool;
+
 public:
     HttpServer(const uint16_t port = PORT)
-        : _port(port), _tcp_server(nullptr), _stop(false) // _stop 标识默认服务不会停止！
+        : _port(port) /* ,_tcp_server(nullptr)【引入线程池：删】*/ 
+        ,_stop(false) // _stop 标识默认服务不会停止！
     {
     }
 
     void InitHttpServer()
     {
-        _tcp_server = TcpServer::GetInstance(_port);
+        // 设置忽略管道对端关闭OS给我们的信号【避免该情况导致服务器挂了】
+        signal(SIGPIPE, SIG_IGN);
+        // _tcp_server = TcpServer::GetInstance(_port);
     }
 
     void Loop()
     {
-        /* 获取套接字 */
-        int listensock = _tcp_server->GetSock();
+        TcpServer *_tcp_server = TcpServer::GetInstance(_port);
+        /* 获取套接字 【引入线程池：删】*/
+        // int listensock = _tcp_server->GetSock();
         LogMessage(INFO, "TcpServer launch ...");
 
         while (!_stop)
@@ -33,27 +54,24 @@ public:
             /* 获取网络链接 */
             struct sockaddr_in peer;
             socklen_t len = sizeof(peer);
-            int sock = accept(listensock, (struct sockaddr *)&peer, &len);
+            int sock = accept(_tcp_server->GetSock(), (struct sockaddr *)&peer, &len);
             if (sock < 0)
             {
                 // 获取失败！
             }
             LogMessage(INFO, "Get a new link ...");
-            /* 重新创建一个拷贝的sock，防止在多线程访问下的数据异常 */
-            int *_sock = new int(sock);
-            /* 创建线程：执行任务 */
-            pthread_t tid;
-            pthread_create(&tid, nullptr, Entrence::HandlerRequest, (void *)_sock);
-            /* 线程分离：实现无需等待！执行完任务后自动释放资源！ */
-            pthread_detach(tid);
+
+            // 引入线程池
+            Task task(sock);
+            ThreadPool::GetInstance()->PushTask(task);
+
+            // /* 重新创建一个拷贝的sock，防止在多线程访问下的数据异常 */
+            // int *_sock = new int(sock);
+            // /* 创建线程：执行任务 */
+            // pthread_t tid;
+            // pthread_create(&tid, nullptr, Entrence::HandlerRequest, (void *)_sock);
+            // /* 线程分离：实现无需等待！执行完任务后自动释放资源！ */
+            // pthread_detach(tid);
         }
     }
-
-private:
-    /* 端口号 */
-    uint16_t _port;
-    /* 网络服务实例 */
-    TcpServer *_tcp_server;
-    /* 服务是否可停止 */
-    bool _stop;
 };
